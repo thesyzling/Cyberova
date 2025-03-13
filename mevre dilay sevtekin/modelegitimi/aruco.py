90531965fd38207a2553d0e534c03d4e565f01d2
Merve Dilay <mervedilaysevtekin@gmail.com>
2025-03-14 02:27:12 +0300

    Update aruco.py

--- mevre dilay sevtekin/modelegitimi/aruco.py
+++ mevre dilay sevtekin/modelegitimi/aruco.py
@@ -1,1 +1,34 @@
-C:\Users\merve dilay sevtekin\arUco.py\arUco.py\aruco.py
+import cv2
+import cv2.aruco as aruco
+import numpy as np
+
+# ArUco dictionary seçimi
+aruco_dict = aruco.getPredefinedDictionary(aruco.DICT_5X5_250)
+parameters = aruco.DetectorParameters()
+
+
+cap = cv2.VideoCapture(0)
+
+while True:
+    ret, frame = cap.read()
+    if not ret:
+        print("Kamera görüntüsü alinamiyor.")
+        break
+
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY) 
+    detector = aruco.ArucoDetector(aruco_dict, parameters)
+    corners, ids, _ = detector.detectMarkers(gray)  
+
+    if ids is not None:
+        aruco.drawDetectedMarkers(frame, corners, ids)  
+
+    cv2.imshow("ArUco Marker Detection", frame)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'): 
+        break
+
+cap.release()
+cv2.destroyAllWindows()
+
+
+
