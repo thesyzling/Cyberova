b7464a418b6c061d72dd5d34ac24967887810031
Barış <129172437+thesyzling@users.noreply.github.com>
2025-07-06 20:20:50 +0300

    Delete Sevilay_Celik directory

--- Sevilay_Celik/opencv/1.pyth.py
+++
@@ -1,9 +0,0 @@
-import cv2
-img=cv2.imread("audi.jpg"cv2.IMREAD_GRAYSCALE)
-# IMREAD_GRAYSCALE yerine 0 kullanabiliriz
-cv2.namedWindow("image",cv2.WINDOW_NORMAL)
-cv2.imshow("image",img)
-cv2.imwrite("copy.jpg",img)
-cv2.waitKey(0)
-
-cv2.destroyAllWindows()
