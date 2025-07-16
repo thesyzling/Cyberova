d00450062ff888388910d4adc5d05e4e3cfd31af
Hilal Ateş <ateshilal3344@gmail.com>
2025-07-16 17:20:05 +0300

    Delete build directory

--- build/last_smooth_controller/colcon_command_prefix_setup_py.sh
+++
@@ -1,2 +0,0 @@
-# generated from colcon_core/shell/template/command_prefix.sh.em
-. "/home/hilal/ros2_ws/install/last_description/share/last_description/package.sh"
--- build/rplidar_slam/CMakeFiles/rplidar_slam_uninstall.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/rplidar_slam_uninstall"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/rplidar_slam_uninstall.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
